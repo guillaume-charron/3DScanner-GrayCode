e0270cfc576579b34d7e7867b88495f94600e95d
Guillaume Charron <guillaume.charron99@hotmail.com>
2023-11-23 16:23:49 -0500

    Add calibrate camera code

--- GrayCode/generate_codes.py
+++ GrayCode/generate_codes.py
@@ -12,7 +12,7 @@ def get_gray_codes(n):
         codes.append(s.zfill(n))
     return codes
 
-def display_gray_code(gray_codes, width, height, repeat_n):
+def display_gray_code(gray_codes, width, height, repeat_n, fps):
     # Create a black image
     image = np.zeros((4*len(gray_codes[0]),height, width), dtype=np.uint8)
 
@@ -37,17 +37,17 @@ def display_gray_code(gray_codes, width, height, repeat_n):
             image[4*(len(code) - (j+1)) + 2, start_row:end_row, :] = 255 if bit == '1' else 0
             image[4*(len(code) - (j+1)) + 3, start_row:end_row, :] = 255-image[4*(len(code) - (j+1)) + 2, start_row:end_row, :]
     
-    out = cv2.VideoWriter('gray_sequence.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 5, (width, height), isColor=False)
+    #out = cv2.VideoWriter('gray_sequence.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 5, (width, height), isColor=False)
  
-    #for n in range(repeat_n):
-    for i in range(len(image)):
+    for n in range(repeat_n):
+        for i in range(len(image)):
         # Display the image
-        out.write(image[i])
+        #out.write(image[i])
         
-        #cv2.imshow("Gray Code Pattern", image[i])
-        #cv2.waitKey(100)
-    #cv2.destroyAllWindows()
-    out.release()
+            cv2.imshow("Gray Code Pattern", image[i])
+            cv2.waitKey(int(1000/fps))
+    cv2.destroyAllWindows()
+    #out.release()
 
 
 # Get gray codes
@@ -56,4 +56,5 @@ gray_codes_width = get_gray_codes(9)
 # Display gray codes
 width, height = (1920, 1080)
 repeat_n = 5
-display_gray_code(gray_codes_width, width, height, repeat_n)
+fps = 10
+display_gray_code(gray_codes_width, width, height, repeat_n, fps)
