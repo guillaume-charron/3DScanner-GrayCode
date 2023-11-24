358acb5455607626a9bd300b9e591ddf277d3752
Guillaume Charron <guillaume.charron99@hotmail.com>
2023-11-24 12:48:42 -0500

    Simplify gray code generation

--- GrayCode/generate_codes.py
+++ GrayCode/generate_codes.py
@@ -1,60 +1,70 @@
 import cv2
 import numpy as np
 
-# took from https://www.geeksforgeeks.org/generate-n-bit-gray-codes/
-def get_gray_codes(n):
-    codes = []
+# Inspired from https://github.com/sjnarmstrong/gray-code-structured-light/blob/master/CaptureImages/GrayImages.py
+def get_gray_codes(width, height):
+    # Get larger size
+    max_size = max(width, height)
 
-    for i in range(1 << n):
-        val = (i ^ (i >> 1))
-        s = bin(val)[2::]
+    # Get length of gray codes to cover all pixels
+    n_bits = int(np.ceil(np.log2(max_size)))
 
-        codes.append(s.zfill(n))
-    return codes
+    # Generate Gray codes
+    codes = np.arange(max_size, dtype=np.uint16)
+    codes = (codes >> 1) ^ codes
+    codes.byteswap(inplace=True)
 
-def display_gray_code(gray_codes, width, height, repeat_n, fps):
+    return np.unpackbits(codes.view(dtype=np.uint8)).reshape((-1, 16))[:, 16-n_bits:]
+
+def display_gray_code(gray_codes, width, height, repeat_n, fps, write_video_seq=False):
     # Create a black image
     image = np.zeros((4*len(gray_codes[0]),height, width), dtype=np.uint8)
 
     # Calculate the size each stripe
-    stripe_width = width / (len(gray_codes))
-    stripe_height = height / (len(gray_codes))
+    stripe_size = width // (len(gray_codes))
     
     # Draw the Gray code pattern
     for i, code in enumerate(gray_codes):
         for j, bit in enumerate(code):
-            start_col = int(np.round(i * stripe_width))
-            end_col = int(np.round((i + 1) * stripe_width))
-            start_row = int(np.round(i * stripe_height))
-            end_row = int(np.round((i + 1) * stripe_height))
+            start_pos = i * stripe_size
+            end_pos = (i + 1) * stripe_size
 
-            if i == len(gray_codes) - 1:
-                end_col = width
-                end_row = height
-            image[4*j, :, start_col:end_col] = 255 if bit == '1' else 0
-            image[4*j+1, :, start_col:end_col] = 255-image[4*j, :, start_col:end_col]
-        
-            image[4*(len(code) - (j+1)) + 2, start_row:end_row, :] = 255 if bit == '1' else 0
-            image[4*(len(code) - (j+1)) + 3, start_row:end_row, :] = 255-image[4*(len(code) - (j+1)) + 2, start_row:end_row, :]
+            # Compute ids of frames, we want somthing like X9+,Y0+,X8+,Y1+,...,X9-,Y0-,X8-,Y1-,..
+            # where + is normal and - is inverse
+            id_v = 2*j
+            id_h = 2*(len(code) - (j+1)) + 1
+            id_inv_v = 2*j + 2*len(code)
+            id_inv_h = 2*(len(code) - (j+1)) + 1 + 2*len(code)
+
+            image[id_v, :, start_pos:end_pos] = 255 if bit == 1 else 0
+            image[id_inv_v, :, start_pos:end_pos] = 255-image[id_v, :, start_pos:end_pos]
+            if i < height - 1:
+                image[id_h, start_pos:end_pos, :] = 255 if bit == 1 else 0
+                image[id_inv_h, start_pos:end_pos, :] = 255-image[id_h, start_pos:end_pos, :]
     
-    #out = cv2.VideoWriter('gray_sequence.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 5, (width, height), isColor=False)
+    if write_video_seq:
+        out = cv2.VideoWriter('./data/gray_sequence.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height), isColor=False)
  
     for n in range(repeat_n):
         for i in range(len(image)):
-        # Display the image
-        #out.write(image[i])
-        
+            # Display the image
             cv2.imshow("Gray Code Pattern", image[i])
             cv2.waitKey(int(1000/fps))
+
+            if write_video_seq and n == 0:
+                out.write(image[i])
+    
     cv2.destroyAllWindows()
-    #out.release()
+
+    if write_video_seq:
+        out.release()
 
 
 # Get gray codes
-gray_codes_width = get_gray_codes(9)
+width, height = (1920, 1080)
+gray_codes = get_gray_codes(width, height)
 
 # Display gray codes
-width, height = (1920, 1080)
-repeat_n = 5
+repeat_n = 1
 fps = 10
-display_gray_code(gray_codes_width, width, height, repeat_n, fps)
+display_gray_code(gray_codes, width, height, repeat_n, fps, write_video_seq=True)
